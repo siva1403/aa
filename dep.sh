3d06819c88b13195acd659a5f128a38a29928737
siva1403 <k.s.krishnajob@gmail.com>
2025-12-25 14:24:38 +0530

    d

---
+++ dep.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+sudo apt-get update
+sudo apt-get install -y openjdk-11-jre
+sudo apt-get install -y maven
+mvn clean install
+sudo apt-get install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+docker build -t my-java-app .
