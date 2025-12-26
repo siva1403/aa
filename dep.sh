8f4e1aba9ec65693b154742a1ea9a6348c8cb294
siva1403 <k.s.krishnajob@gmail.com>
2025-12-26 18:44:23 +0530

    compress

--- dep.sh
+++ dep.sh
@@ -2,6 +2,8 @@
 sudo apt-get update
 sudo apt-get install -y openjdk-11-jre
 sudo apt-get install -y maven
+sudo apt-get install -y curl
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v0.57.0
 mvn clean install
 sudo apt-get install -y docker.io
 sudo systemctl start docker
